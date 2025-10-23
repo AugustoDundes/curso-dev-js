b7bb34ba63157c1981be9a8b62ffee4228be881f
Augusto Dundes Parucci <Augusto Dundes Parucci>
2025-10-22 21:41:15 -0300

    com projeto react

---
+++ exercicios-individuais/vite-project/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          add count
+        </button>
+        <p>count is {count}</p>
+        <button onClick={() => setCount((count) => count - 1)}>
+          decrement
+        </button>
+        <br/>
+        <button onClick={() => setCount(0)}>  
+          reset count
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
